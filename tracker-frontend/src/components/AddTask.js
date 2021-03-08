dc702db790a3b91b15ab497ac5d62ac1e13510eb
Madlen Engel <engel.madlen@googlemail.com>
2021-03-08 11:41:25 +0100

    Create addTask button and modal

--- tracker-frontend/src/components/AddTask.js
+++ tracker-frontend/src/components/AddTask.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
-const AddTask = () => {
+const AddTask = ({time}) => {
+    const [task, setTask] = useState({duration: {time}, description: "", timestamp: 0})
+    const [open, setOpen] = useState(false);
+    
+    const customStyles = {
+        content : {
+            backgroundColor: 'black',
+            minWidth: '60%',
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            padding: '30px',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+        }
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+    const handleClose = () => {
+    setOpen(false);
+    };
+
     return(
-        <div class="d-grid gap-2 col-6 mx-auto my-5">
-            <h2 className="d-flex mx-auto my-5">
-                Add task
-            </h2>
+        <div >
+            <button type="button" className="btn btn-danger my-2" style={{ width: "100%" }} onClick={handleOpen}>
+                    Add Task
+            </button>
+
+            <Modal isOpen={open} style={customStyles} onRequestClose={handleClose}>
+                <h2>Add Task</h2>
+                <div>Duration: 
+                    <span>{(time.h >= 10)? time.h : "0"+ time.h}</span>: 
+                    <span>{(time.m >= 10)? time.m : "0"+ time.m}</span>:
+                    <span>{(time.s >= 10)? time.s : "0"+ time.s}</span></div>
+                <form>
+                    
+                </form>
+                <button type="button" class="btn btn-primary m-2" onClick={handleClose}>Close</button>
+            </Modal>
         </div>
     )
 };
